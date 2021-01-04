3b9257ac5c9716b8b6e741f840cba3bbff0512cc
Alexandre BOREL <alexandreborel1994@gmail.com>
2021-01-04 18:50:00 +0100

    we forgot dist/

---
+++ dist/precache-manifest.b5f21c3bf2a257a76140cf26948e5b75.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920f7af1073da174b9bd",
+    "url": "/css/app.674d2a6b.css"
+  },
+  {
+    "revision": "8d8230782a6a10dbf7095fde95fa1c0c",
+    "url": "/img/discord/login_1.png"
+  },
+  {
+    "revision": "fad4979a2b340853a22cc5ad55b27004",
+    "url": "/img/disney-plus/login_1.png"
+  },
+  {
+    "revision": "9645ec1e4e8754013638038edfb8593e",
+    "url": "/img/disney-plus/login_2.png"
+  },
+  {
+    "revision": "768bbf70f1bb7ae42466f87fbf4cc395",
+    "url": "/img/facebook/login_1.png"
+  },
+  {
+    "revision": "7981b5eb2e3c81f267450d3a37a8991a",
+    "url": "/img/instagram/login_web_1.png"
+  },
+  {
+    "revision": "75efb2c8a04c01b561174a5682e7344c",
+    "url": "/img/netflix/login_1.jpg"
+  },
+  {
+    "revision": "287faf9c261e28ce07a5b20b27684daa",
+    "url": "/img/snapchat/login_1.png"
+  },
+  {
+    "revision": "ae0c6ae8d69b9059451a70ca762ba38a",
+    "url": "/img/steam/login_1.png"
+  },
+  {
+    "revision": "1077b015a3581b0c81636ff2b630064d",
+    "url": "/img/twitter/login_1.png"
+  },
+  {
+    "revision": "9a0fce2484122319c6422f64c82e525b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c6d3e6f1a3e239d2a4",
+    "url": "/js/about.cec9c08b.js"
+  },
+  {
+    "revision": "920f7af1073da174b9bd",
+    "url": "/js/app.03369263.js"
+  },
+  {
+    "revision": "d8a9e8e5af3de185685b",
+    "url": "/js/chunk-vendors.3dfea2f6.js"
+  },
+  {
+    "revision": "5fef7bda9fecd042a445e5adcb02dc57",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
